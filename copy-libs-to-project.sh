e1d31154225752bb907d72b9a0166cdaf75d0c4b
victormak <victormak357@gmail.com>
2016-03-01 16:33:18 +0800

    modify copy-libs-to-project.sh

--- copy-libs-to-project.sh
+++ copy-libs-to-project.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cp ./app/src/main/libs/arm64-v8a/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/arm64-v8a
-cp ./app/src/main/libs/armeabi/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi
-cp ./app/src/main/libs/armeabi-v7a/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi-v7a
-cp ./app/src/main/libs/x86/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/x86
+cp -f ./app/src/main/libs/arm64-v8a/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/arm64-v8a
+cp -f ./app/src/main/libs/armeabi/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi
+cp -f ./app/src/main/libs/armeabi-v7a/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi-v7a
+cp -f ./app/src/main/libs/x86/libiconvx.so ../AndroidDemo/app/src/main/jni/myLibiconvx/x86
 
-cp ./app/src/main/libs/arm64-v8a/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/arm64-v8a
-cp ./app/src/main/libs/armeabi/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi
-cp ./app/src/main/libs/armeabi-v7a/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi-v7a
-cp ./app/src/main/libs/x86/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/x86
+cp -f ./app/src/main/libs/arm64-v8a/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/arm64-v8a
+cp -f ./app/src/main/libs/armeabi/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi
+cp -f ./app/src/main/libs/armeabi-v7a/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/armeabi-v7a
+cp -f ./app/src/main/libs/x86/libxml2x.so ../AndroidDemo/app/src/main/jni/myLibiconvx/x86
 
 
